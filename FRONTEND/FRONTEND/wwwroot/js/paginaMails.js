7a16c990fbe19ca2dc951eecef58936bf526a89e
viksen <viksen_1234@hotmail.com>
2021-03-06 18:21:56 +0100

    Abrir correos recibidos funciona

--- FRONTEND/FRONTEND/wwwroot/js/paginaMails.js
+++ FRONTEND/FRONTEND/wwwroot/js/paginaMails.js
@@ -2,25 +2,35 @@
 var b = document.getElementById("newMailButtons");
 var x = document.getElementById("hijoPrincipal");
 var y = document.getElementById("hijoCorreoNuevo");
+var w = document.getElementById("hijoMailRecibido");
 var rec = document.getElementById("recibidos");
 var cN = document.getElementById("correoNuevo");
 
-function MostrarMail() {
+function MostrarMailNuevo() {
     if (y.style.display === "none") {
         y.style.display = "flex";
         x.style.display = "none";
+        w.style.display = "none";
         a.style.display = "none";
         b.style.display = "flex";
     }
 }
-function MostrarRecibidos() {
+function MostrarBandejaEntrada() {
     if (x.style.display === "none") {
         x.style.display = "flex";
         y.style.display = "none";
+        w.style.display = "none";
         b.style.display = "none";
         a.style.display = "flex";
     }
 }
+function MostrarMailRecibido() {
+    if (w.style.display === "none") {
+        w.style.display = "flex";
+        x.style.display = "none";
+    }
+}
+
 
 function getMailTrabajadores(token) {
     $.ajax({
